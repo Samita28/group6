506f6c23bbf31cdfc0295f97f284c57703eacbb7
Samita28 <73217405+Samita28@users.noreply.github.com>
2020-11-24 13:55:35 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get("/main/:id", async function (req, res) {
             });
         }
         else if (j.data.type == "employee"){
-            res.render("main",{
+            res.render("mainemployee",{
             prefix: j.data.prefixname,
             name_th: j.data.displayname_th,
             name_en: j.data.displayname_en,
@@ -55,6 +55,9 @@ app.get("/main/:id", async function (req, res) {
 app.get('/main', function (req, res) {
     res.render('main')
 });
+app.get('/mainemployee', function (req, res) {
+    res.render('mainemployee')
+});
 app.get('/status', function (req, res) {
     res.render('status')
 });
@@ -67,6 +70,18 @@ app.get('/Fill-up', function (req, res) {
 app.get('/results', function (req, res) {
     res.render('results')
 });
+app.get('/pay', function (req, res) {
+    res.render('pay')
+});
+app.get('/advisor', function (req, res) {
+    res.render('advisor')
+});
+app.get('/teacher', function (req, res) {
+    res.render('teacher')
+});
+app.get('/dean', function (req, res) {
+    res.render('dean')
+});
 app.post("/api", async (req, res) => {
 
     console.log(req.body);
@@ -76,9 +91,32 @@ app.post("/api", async (req, res) => {
         let j = JSON.parse(temp);
         console.log(j);
         if (j.status == true) {
-            res.send(temp);
-        } else {
-            res.send('{"status":false}');
+            if(j.type == "student"){
+        res.render("main",
+            {
+                prefix: j.prefixname,
+                name_th: j.displayname_th,
+                name_en: j.displayname_en,
+                email: j.email,
+                faculty: j.faculty,
+                department: j.department
+            });
+        }
+        else if (j.type == "employee"){
+            res.render("mainemployee",{
+            prefix: j.prefixname,
+            name_th: j.displayname_th,
+            name_en: j.displayname_en,
+            email: j.email,
+            organization: j.organization,
+            department: j.department
+            
+        });
+        }} else {
+            let fails=JSON.parse(temp);
+      res.render('index', {
+        errors: fails.message
+      })
         }
     } else {
         res.send('{"status":false}');
